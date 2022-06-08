312cb20274dafdbc434c1d3d0f389058f60db15c
@yush P@nwar <ayushpanwar691@gmail.com>
2022-06-08 22:27:48 +0530

    Weekly problems

--- Practical/week 9/mergeCost.cpp
+++ Practical/week 9/mergeCost.cpp
@@ -1,20 +1,26 @@
 #include<iostream>
 #include<queue>
+#include<algorithm>
 using namespace std;
+
 int main(){
-  int n,compute=0,a,b;
-  cin>>n;
-  int s[n];
-  priority_queue<int,vector<int>,greater<int>>pq;
-  for(int &i:s)cin>>i,pq.push(i);
-  while(pq.size()>1){
-    a=pq.top();
-    pq.pop();
-    b=pq.top();
-    pq.pop();
-    compute+=a+b;
-    cout<<a+b<<endl;
-    pq.push(a+b);
-  }
-  cout<<compute;
+    int n,a;
+    cin>>n;
+    priority_queue<int ,vector<int>,greater<int>> minheap;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        minheap.push(a);
+    }
+    int res=0;
+    while(minheap.size()>1){
+        int e1 = minheap.top();
+        minheap.pop();
+        int e2 = minheap.top();
+        minheap.pop();
+        int temp=e1+e2;
+        res+=temp;
+        minheap.push(e1+e2);
+    }
+    cout<<res;
+    return 0;
 }
